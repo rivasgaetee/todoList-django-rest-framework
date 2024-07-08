60042f9fe500c8a23217d3e9ad699f3caf920a07
Esteban Rivas <erivga18@gmail.com>
2024-07-08 11:43:39 -0400

    Remove comentarios

--- tasks/tests.py
+++ tasks/tests.py
@@ -5,10 +5,8 @@ from .models import Task, TaskStatus
 class TaskModelTest(TestCase):
 
     def setUp(self):
-        # Crear un estado inicial para las tareas
         self.task_status = TaskStatus.objects.create(name="not_started")
 
-        # Crear una tarea de ejemplo
         self.task = Task.objects.create(
             title="Test Task",
             description="Esto es una prueba de Tarea",
@@ -25,5 +23,4 @@ class TaskModelTest(TestCase):
         self.assertIsNotNone(self.task.updated_at)
 
     def test_task_string_representation(self):
-        # Verificar la representación en cadena de la tarea
         self.assertEqual(str(self.task), "Test Task")
